784b23a708e97ce728fa8b9be810bb138a286a34
sumitjhingade <127714607+sumitjhingade@users.noreply.github.com>
2026-02-16 16:09:08 +0530

    Implement SIP calculator using Flask and Pandas

---
+++ sip_calculator.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import Flask,render_template,request,Response
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    new_row = pd.DataFrame(columns=['Year', 'Invested_Value','Value'])
+
+    if request.method == 'POST':
+        Amount = int(request.form['Amount'])
+        Duration  = int(request.form['Durations'])
+        Returns  = int(request.form['Returns'])
+        roi = (Returns)/120
+        fund_value = 0
+        
+        for i in range(1,Duration+1):
+            months= i*12
+            row = []
+            row.append(i)
+            invested_value = Amount*12*i
+            row.append(invested_value)
+            #new_value= round(Amount*(1+(roi/12))**(12*i))
+            new_value = round(Amount * ((((1 + roi)**12) - 1) / roi) * (1 + roi))
+            fund_value += new_value
+            row.append(fund_value)
+            new_row.loc[len(new_row)] = row
+        html_table = new_row.to_html(index=False, classes="table table-striped")    
+        return html_table
+                       
+    return render_template('form.html')
+
+if __name__ == '__main__':
+
+
+    app.debug = True
+    app.run()
+
